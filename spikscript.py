e2644d89168fb82c81543f765dd8042918cec1ae
Tiago <tiago.marcos@gmx.co.uk>
2018-08-10 11:49:48 -0400

    Create spikscript.py
    spike-generator

---
+++ spikscript.py
@@ -0,0 +1,30 @@
+n#created by TS Marcos
+#generate spikes based on channel population
+
+import Tkinter as Tk
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+
+t = time()
+init = 0.25+t
+st = raw_input("Stimulus")
+
+for (i in 1:20) {
+print ("The number is \i")
+}
+
+Na(ct) = 0.001
+K(ct) = 0.005
+Hcn(ct) = -0.001
+
+GSK = (Na(ct)+K(ct)/3)/Na(ct)*5 # mVolts
+
+x = c(1:10000) #timevector
+
+plt.plot(x,y, col="blue", type="l")
+plt.show() #show me the graphs
+#save automatically
+
+
+
